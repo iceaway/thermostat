be00dd46cfbec0a2ea8b8e13271515c782d66011
Pelle Windestam <pelle.windestam@tagmaster.com>
2017-12-01 07:59:51 +0100

    new experimental temp calculation based on lookup table

--- temperature.c
+++ temperature.c
@@ -50,6 +50,34 @@ static float adc2temp(uint16_t val)
   return temp;
 }
 
+struct rt {
+  int32_t res;
+  int32_t t;
+};
+
+struct rt rt_table[] = {
+  { 437139, -55 },
+  { 246574, -45 },
+  { 143739, -35 },
+  { 86390,  -25 },
+  { 53417,  -15 },
+  { 33916,  -5  },
+  { 27280,  0,  },
+  { 22073,  5   },
+  { 14701,  15  },
+  { 10000,  25  },
+  { 6946,   35  },
+  { 4914,   45  },
+  { 3538,   55  },
+  { 2589,   65  },
+  { 1924,   75  },
+  { 1451,   85  },
+  { 1108,   95  },
+  { 857,    105 },
+  { 671,    115 },
+  { 531,    125 }
+};
+
 static uint32_t adc2res(uint16_t adc)
 {
   uint32_t res;
@@ -57,6 +85,34 @@ static uint32_t adc2res(uint16_t adc)
 
   res = RES*SCALE_FACTOR/tmp;
   return res;
+}
+
+static int32_t adc2temp_v2(uint16_t adc)
+{
+  int32_t res = adc2res(adc);
+  unsigned int i = 0; 
+  int32_t k, m, t;
+
+  while ((rt_table[i].res >= res) && (i < sizeof(rt_table)/sizeof(rt_table[0]))) {
+    if (res == rt_table[i].res)
+      return rt_table[i].t;
+    ++i;
+  }
+
+  if (i == sizeof(rt_table)/sizeof(rt_table[0]))
+    return 0;
+
+  if (i) {
+    k = (rt_table[i].t - rt_table[i-1].t) / (rt_table[i].res - rt_table[i-1].res);
+    m = rt_table[i].t - k * rt_table[i].res;
+    t = k*res + m;
+    prints(PSTR("Temperature is: %ld\r\n"), t);
+    return t;
+  } else {
+    prints(PSTR("Invalid resistance: %lu\r\n"), res);
+  }
+
+  return 0;
 }
 
 float temperature_get(void)
